693b9a1e345de81edde94383b583cfe31d87997e
YSHebron <yshebron@up.edu.ph>
2023-07-05 16:24:48 +0800

    feat: prepped kmer-analysis for project-specific data
    TODO: revise kmer-analysis for repo set-up

--- code/kmer-analysis.R
+++ code/kmer-analysis.R
@@ -1,11 +1,28 @@
+# Reference: https://github.com/ai-covariants/analysis-mutations
+# File: kmer-analysis.R
 
-#Import all the required libraries
-library(ape)
-library(kmer)
-library("dplyr")
+# INSTALL AND LOAD PACKAGES ################################
 
-#set the working directory 
-setwd("")
+library(datasets)  # Load base packages manually
+
+# Installs pacman ("package manager") if needed
+if (!require("pacman"))
+  install.packages("pacman", repos = "https://cran.case.edu")
+library(pacman)
+
+# Use pacman to load add-on packages as desired
+# First call are standard packages, second call are file specific packages.
+pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes, 
+               ggvis, httr, lubridate, plotly, psych,
+               rio, rmarkdown, shiny, 
+               stringr, tidyr, tidyverse)
+pacman::p_load(ape, kmer)
+
+# LOAD DATA ################################################
+# Set working directory to container of fasta and metadata files
+setwd("data/GISAID/datasets")
+
+# WORK WITH DATA ###########################################
 
 #Function computes the kmer of given length 
 kmer_df = function(filePath,variant,k){
@@ -32,3 +49,20 @@ for(k in kmer_list)
   #store the combined data in a csv file 
   write.csv(data, outputName)
 }
+
+# CLEAN UP #################################################
+
+# Clear environment
+rm(list = ls()) 
+
+# Clear packages
+p_unload(all)  # Remove all add-ons
+detach("package:datasets", unload = TRUE)  # For base
+
+# Clear plots but only if there IS a plot
+while (!is.null(dev.list())) dev.off()
+
+# Clear console
+cat("\014")  # ctrl+L
+
+# Clear mind :)
