76f68f87591cfe582391938f659332995ebb80fc
YSHebron <yshebron@up.edu.ph>
2023-07-06 09:53:48 +0800

    feat: kmer-analysis.R random sampler done

--- code/kmer-analysis.R
+++ code/kmer-analysis.R
@@ -43,63 +43,100 @@ if (dir.exists(extractPath)) {
   }
 }
 
-# DEFINE FUNCTIONS ###########################################
+# Merge all extracted fasta and tsv files
+# fasta contains sequence, while tsv contains meta
 omicron_sub = c('ba275', 'xbb', 'xbb_1.5', 'xbb_1.16', 'xbb1.91')
 
-# Function computes the kmer of given length, returns a data frame
-kmer_df <- function(fastaPath, tsvPath, variant, k){
+fastas <- list.files(extractPath, recursive = TRUE, pattern = '.+\\.fasta')
+tsvs <- list.files(extractPath, recursive = TRUE, pattern = '.+\\.tsv')
+nfiles <- length(fastas)
+
+fastaAll <- data.frame()
+metaDataAll <- data.frame()
+
+for (i in 1:nfiles) {
+  fastaPath <- paste(extractPath, fastas[i], sep='/')
+  tsvPath <- paste(extractPath, tsvs[i], sep='/')
+  variant <- str_match(fastaPath, pattern = '(?<=-).*(?=\\/)')
+  if (variant %vin% omicron_sub) {
+    variant <- 'omicron_sub'
+  }
+  print(paste("Reading", fastaPath))
+  print(paste("Reading", tsvPath))
+  
+  # Merge fasta file with accumulator
   fasta <- read.FASTA(fastaPath)
-  meta <- as.data.frame(read_tsv(tsvPath, col_select = c(1,2,5,10,11,12,14,16,17,19)))
+  fastaAll <- c(fastaAll, fasta)
+  
+  # Merge metaData file with accumulator
+  # Defer sanitation after random sampling so fasta and metaData maintains 1:1
+  metaData <- as.data.frame(read_tsv(tsvPath,
+                                     col_select = c(1,5,10,11,12,14,16,17,19)))
   # Not removing raw date as I believe it is useful for sorting or can be parsed on an as needed basis
-  meta <- meta %>%
+  metaData <- metaData %>%
     dplyr::mutate(year = lubridate::year(date),
                   month = lubridate::month(date),
-                  day = lubridate::day(date))
+                  day = lubridate::day(date),
+                  variant = variant)
+  
+  metaData$length <- as.integer(metaData$length)
+  metaData$age <- as.integer(metaData$age)
+  metaData$year <- as.integer(metaData$year)
+  metaData$month <- as.integer(metaData$month)
+  metaData$day <- as.integer(metaData$day)
+  
+  metaDataAll <- bind_rows(metaDataAll, metaData)
+}
+
+rm(fasta)
+rm(metaData)
+
+# At this point, fastaAll and metaDataAll contains the needed data
+# Now do random sampling
+
+M <- length(fastaAll)
+N <- nrow(metaDataAll)
+# Guard condition, M == N
+if (M != N) {
+  print("Warning: size of fasta != metaData")
+}
+idxs <- sample(1:M, 500, replace = FALSE)
+fastaAll <- fastaAll[idxs]
+metaDataAll <- metaDataAll[idxs,]
+
+# Drop rows with NA values and type mismatches
+# For dropping, get the idxs of the dropped rows and also drop them to in fastaAll
+drop_idxs <- which(is.na(metaDataAll), arr.ind=TRUE)[,1]
+# drop_idxs <- c(drop_idxs, as.vector(which(is.numeric(metaDataAll$sex))))
+# drop_idxs <- c(drop_idxs, which(!(metaDataAll$sex %vin% list("Male", "Female"))))
+drop_idxs <- unique(drop_idxs)
+drop_idxs
+
+fastaAll <- fastaAll[is.na(pmatch(1:500, drop_idxs))]
+metaDataAll <- metaDataAll[is.na(pmatch(1:500, drop_idxs)),]
+
+# Returns a data frame of kmers
+kmer <- function(fasta, metaData, k){
   kmers <- kcount(fasta, k = k)
-  target <- rep((variant),length(kmers))
-  kmer_df <- data.frame(kmers, target)
+  kmer_df <- data.frame(kmers)
+  
+  print(dim(kmer_df))
+  print(dim(metaData))
   
   # Append meta
-  kmer_df <- cbind(kmer_df, meta)
+  kmer_df <- cbind(kmer_df, metaData)
   
   return(kmer_df)
 }
 
 # WORK WITH DATA ###########################################
 
-# Different lengths of kmers to be used 
-kmer_list <- list(3)
 
-# Instantiate alpha accumulators
-
-for(k in kmer_list) {
-  # fasta contains sequence, while tsv contains meta
-  fastas <- list.files('data/GISAID/datasets', recursive = TRUE, pattern = '.+\\.fasta')
-  tsvs <- list.files('data/GISAID/datasets', recursive = TRUE, pattern = '.+\\.tsv')
-  nfiles <- length(fastas)
-  # instantiate accumulator
-  data <- data.frame()
-  for (i in 1:nfiles) {
-    fastaPath <- paste(extractPath, fastas[i], sep='/')
-    tsvPath <- paste(extractPath, tsvs[i], sep='/')
-    variant <- str_match(fastaPath, pattern = '(?<=-).*(?=\\/)')
-    if (variant %vin% omicron_sub) {
-      variant <- 'omicron_sub'
-    }
-    print(paste("Reading", fastaPath))
-    print(paste("Reading", tsvPath))
-    temp <- kmer_df(fastaPath, tsvPath, variant, k)
-    data <- bind_rows(data, temp)
-  }
-  outputName = sprintf("kmer_%d.csv", k)
-  # #store the combined data in a csv file 
-  write.csv(paste('data/kmers'), outputName)
-}
 
 # CLEAN UP #################################################
 
 # Clear environment
-# rm(list = ls()) 
+# rm(list = ls())
 
 # Clear packages
 # p_unload(all)  # Remove all add-ons
