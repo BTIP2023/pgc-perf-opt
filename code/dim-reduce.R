1a3af6c9bd8074656a50d574b56ec7bce9f833f1
d1w1rm4 <bblogrono@up.edu.ph>
2023-07-11 15:32:30 +0800

    Added code for combined PCA and refactored code to use PCA results in t-SNE

--- code/dim-reduce.R
+++ code/dim-reduce.R
@@ -47,7 +47,7 @@ pca_fn <- function(data, k) {
   explained_variance_1 <- round(summary(pca)$importance[2, 1] / sum(summary(pca)$importance[2, ]), 4)
   explained_variance_2 <- round(summary(pca)$importance[2, 2] / sum(summary(pca)$importance[2, ]), 4)
   
-  cat('Explained variance:', explained_variance_1, explained_variance_2, '\n')
+  # cat('Explained variance:', explained_variance_1, explained_variance_2, '\n')
   
   fig <- plot_ly(FDF, x = ~`Principal Component 1`, y = ~`Principal Component 2`, color = ~variant) %>%
     add_markers(size = 4, alpha = 0.5) %>%
@@ -63,14 +63,64 @@ pca_fn <- function(data, k) {
   save_dim_reduction_plot("pca", k, fig)
 }
 
-tsne_fn <- function(data, k) {
+pcaPlot <- function(data, color, label, symbol) {
+  slice_col <- which(names(data) == 'strain')
+  X <- data[, 2:(slice_col-1)]
+  target <- data$variant
+  
+  # Exclude columns with zero variance
+  non_zero_var_cols <- apply(X, 2, var) > 0
+  X <- X[, non_zero_var_cols]
+  
+  if (ncol(X) < 2) {
+    stop("Insufficient columns with non-zero variance for PCA.")
+  }
+  
+  x <- scale(X)
+  
+  pca <- prcomp(x, center = TRUE, scale = TRUE)
+  PC <- as.data.frame(pca$x[, 1:10])
+  
+  explained_variances <- summary(pca)$importance[2, 1:10] / sum(summary(pca)$importance[2, ])
+  
+  # cat('Variance of each component:', explained_variances, '\n')
+  
+  PC_values <- seq_along(explained_variances)
+  prop_var <- explained_variances
+  
+  df <- data.frame(PC_values, prop_var, color = I(color), label = label, symbol = symbol)
+  
+  return(df)
+}
+
+pca_combined <- function() {
+  df1 <- pcaPlot(data3, '#9400D3', 'k=3', 'circle')
+  df2 <- pcaPlot(data5, '#FF0000', 'k=5', 'star')
+  df3 <- pcaPlot(data7, '#0000FF', 'k=7', 'x')
+  
+  combined_df <- bind_rows(df1, df2, df3)
+  
+  fig <- plot_ly(data = combined_df, x = ~PC_values, y = ~prop_var, type = "scatter", mode = "lines+markers",
+                 color = ~color, name = ~label, symbol = ~symbol, symbols = c("circle", "star", "x"),
+                 marker = list(size = 10)) %>% layout(xaxis = list(title = "Number of Principal Components"),
+                        yaxis = list(title = "Proportion of Variance Explained"))
+  
+  save_dim_reduction_plot("pca", "combined", fig)
+}
+
+tsne_fn <- function(data, k, pca_results = NULL) {
   data <- data[order(data$variant), ]
   slice_col <- which(colnames(data) == 'strain')
   X <- data[, 2:(slice_col - 1)]
   target <- data$variant
   
-  set.seed(10)
-  tsne_results <- Rtsne(X, dims = 2, perplexity = 40, max_iter = 300)
+  if (is.null(pca_results)) {
+    set.seed(10)
+    tsne_results <- Rtsne(X, dims = 2, perplexity = 40, max_iter = 300)
+  } else {
+    tsne_results <- Rtsne(pca_results, dims = 2, perplexity = 40, max_iter = 300, pca = FALSE)
+  }
+  
   df <- data.frame(X1 = tsne_results$Y[, 1], X2 = tsne_results$Y[, 2], target = target)
   p <- plot_ly(df, x = ~X1, y = ~X2, color = ~target, type = "scatter", mode = "markers") %>%
     layout(xaxis = list(title = "TSNE-2D-1"),
@@ -111,7 +161,8 @@ data_list <- list(data3, data5, data7)
 dimensions <- c(3, 5, 7)
 
 for (i in 1:length(data_list)) {
-  pca_fn(data_list[[i]], dimensions[i])
+  pca_combined()
+  pca_results <- pca_fn(data_list[[i]], dimensions[i])
   umap_fn(data_list[[i]], dimensions[i])
-  tsne_fn(data_list[[i]], dimensions[i])
+  tsne_fn(data_list[[i]], dimensions[i], pca_results)
 }
