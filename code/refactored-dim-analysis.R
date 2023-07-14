700fc5648d5698f282fa487b4b9d60268e8430c4
d1w1rm4 <bblogrono@up.edu.ph>
2023-07-14 11:22:10 +0800

    Used ggbiplot to generate biplots

--- code/refactored-dim-analysis.R
+++ code/refactored-dim-analysis.R
@@ -12,16 +12,26 @@ pacman::p_load(
   Rtsne,
   tsne,
   RColorBrewer,
-  ggfortify
+  ggfortify,
+  ggbiplot
 )
 
 # -----Functions-----
 
 # Function for saving plots as PNG and HTML
-save_plot <- function(method, k, p) {
+save_plot <- function(method, k, p, is3D=FALSE) {
   # Save as PNG
   filename <- paste0(method, "-", k, ".png")
   ggsave(filename, p, results_path, device = "png", width=5, height=5, dpi = 300, bg = "white")
+  
+  # TO DO: Fix 3D plotting 
+  # if (is3D) {
+  #   # Convert ggplot object to plotly
+  #   p <- plot_ly(p)
+  # } else {
+  #   # Convert ggplot object to ggplotly
+  #   p <- ggplotly(p)
+  # }
   
   # Convert ggplot object to ggplotly
   p <- ggplotly(p)
@@ -44,11 +54,11 @@ process_data <- function(data) {
   # Check for columns that have zero variance for PCA
   non_zero_var_cols <- apply(x, 2, var) > 0
   x <- x[, non_zero_var_cols]
-
+  
   if (ncol(x) < 2) {
     stop("Insufficient columns with non-zero variance for PCA.")
   }
-
+  
   # # Scale data
   x <- scale(x) # can be optional
   
@@ -75,6 +85,19 @@ Rtsne_fn <- function(pca_results) {
   tsne_df <- Rtsne(pca_results, dims = tsne_dims, perplexity = tsne_perplexity, max_iter = tsne_max_iter, check_duplicate = FALSE, pca = FALSE)
   return(tsne_df)
 }
+
+# # Function for 3D PCA plot
+# pca_3d <- function(pca_df) {
+#   components <- data.frame(pca_df[["x"]])
+#   # components$PC2 <- -components$PC2
+#   # components$PC3 <- -components$PC3
+#   # 
+#   # components = cbind(components, data$variant)
+# 
+# 
+#   # Save plot as PNG and HTML
+#   save_plot("3d-pca", k, components, is3D=TRUE)
+# }
 
 # Function that performs t-SNE (tsne library)
 tsne_fn <- function(pca_results) {
@@ -129,7 +152,7 @@ umap_plot <- function(umap_df, target, k) {
 # -----START-----
 
 # Set k for k-mer analysis
-k <- 3
+k <- 7
 
 # Set paths
 data_path <- "./data/archive/"
@@ -158,12 +181,16 @@ umap_min_dist <- 0.1
 
 # Process the data
 data <- process_data(raw_data)
+target <- data$target
 
 # Perform PCA
 pca_df <- pca_fn(data$x)
 
 # Plot PCA results
 pca_plot(pca_df, raw_data, k)
+
+# Generate 3D PCA plot (unfunctional)
+# pca_3d(pca_df)
 
 # Create screeplot
 p <- fviz_eig(pca_df)
@@ -173,21 +200,28 @@ save_plot("screeplot", k, p)
 p <- fviz_pca_ind(pca_df, 
                   col.ind = "cos2", # Color by the quality of representation
                   gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07")
-                  )
+)
 save_plot("indivgraph", k, p)
 
 # Create graph of variables
 p <- fviz_pca_var(pca_df,
-             col.var = "contrib", # Color by contributions to the PC
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07")
+                  col.var = "contrib", # Color by contributions to the PC
+                  gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07")
 )
 save_plot("vargraph", k, p)
 
 # Create biplot of individuals and variables
-p <- fviz_pca_biplot(pca_df,
-                col.var = "#2E9FDF", # Variables color
-                col.ind = "#696969"  # Individuals color
-)
+# p <- fviz_pca_biplot(pca_df,
+#                 col.var = "#2E9FDF", # Variables color
+#                 col.ind = "#696969"  # Individuals color
+# )
+
+# Create biplot of individuals and variables (using ggbiplot)
+p <- ggbiplot(pca_df, obs.scale = 1, var.scale = 1,
+              groups = target, ellipse = TRUE, circle = TRUE) +
+  scale_color_discrete(name = '') +
+  theme(legend.direction = 'horizontal', legend.position = 'top')
+
 save_plot("biplot", k, p)
 
 # Perform t-SNE(Rtsne library) using PCA results
