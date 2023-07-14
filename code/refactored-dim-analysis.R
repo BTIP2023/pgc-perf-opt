df836a56b7e0dc617f7a1cb5d5e768b700faad40
d1w1rm4 <bblogrono@up.edu.ph>
2023-07-14 08:59:17 +0800

    Added plots from PCA results

--- code/refactored-dim-analysis.R
+++ code/refactored-dim-analysis.R
@@ -10,7 +10,9 @@ pacman::p_load(
   factoextra,
   scales,
   Rtsne,
-  RColorBrewer
+  tsne,
+  RColorBrewer,
+  ggfortify
 )
 
 # -----Functions-----
@@ -19,14 +21,13 @@ pacman::p_load(
 save_plot <- function(method, k, p) {
   # Save as PNG
   filename <- paste0(method, "-", k, ".png")
-  path <- "./results/dim-reduce/R/"
-  ggsave(filename, p, path, device = "png", dpi = 300)
+  ggsave(filename, p, results_path, device = "png", width=5, height=5, dpi = 300, bg = "white")
   
   # Convert ggplot object to ggplotly
   p <- ggplotly(p)
   
   # Save as HTML
-  htmlFile <- paste0("./results/dim-reduce/R/", method, "-", k, ".html")
+  htmlFile <- paste0(results_path, method, "-", k, ".html")
   htmlwidgets::saveWidget(p, file = htmlFile, selfcontained = TRUE)
 }
 
@@ -37,52 +38,48 @@ process_data <- function(data) {
   
   # Determine the columns to use
   slice_col <- which(colnames(data) == "strain")
-  X <- data[, 2:(slice_col - 1)]
+  x <- data[, 2:(slice_col - 1)]
   target <- data$variant
   
   # Check for columns that have zero variance for PCA
-  non_zero_var_cols <- apply(X, 2, var) > 0
-  new_X <- X[, non_zero_var_cols]
-  
-  if (ncol(new_X) < 2) {
+  non_zero_var_cols <- apply(x, 2, var) > 0
+  x <- x[, non_zero_var_cols]
+
+  if (ncol(x) < 2) {
     stop("Insufficient columns with non-zero variance for PCA.")
   }
+
+  # # Scale data
+  x <- scale(x) # can be optional
   
-  # Scale data
-  x <- scale(new_X) # can be optional
-  
-  return(list(X = X, x = x, target = target))
+  return(list(x = x, target = target))
 }
 
 # Function that performs PCA
 pca_fn <- function(x) {
-  pca_df <- prcomp(x, center = TRUE, scale. = TRUE)
+  pca_df <- prcomp(x, center = TRUE)
   return(pca_df)
 }
 
 # Function that plots PCA results
-pca_plot <- function(pca_df, target, k) {
-  # Plot two principal components
-  PC <- as.data.frame(pca_df$x[, 1:2])
-  colnames(PC) <- c("Principal Component 1", "Principal Component 2")
-  FDF <- cbind(PC, variant = target)
-  
-  # Create ggplot object
-  p <- ggplot(FDF, aes(x = `Principal Component 1`, y = `Principal Component 2`, color = variant)) +
-    geom_point(size = 4, alpha = 0.5) +
-    xlab(paste("PC 1 (", round(summary(pca_df)$importance[2,1]  * 100, 2), "% explained variance)")) +
-    ylab(paste("PC 2 (", round(summary(pca_df)$importance[2,2] * 100, 2), "% explained variance)")) +
-    labs(color = "Label") +
-    scale_color_brewer(palette = "Set1")
+pca_plot <- function(pca_df, data, k) {
+  p <- autoplot(pca_df, data = data, colour = 'variant') + scale_color_brewer(palette = "Set1")
   
   # Save plot as PNG and HTML
   save_plot("pca", k, p)
 }
 
-# Function that performs t-SNE
+# Function that performs t-SNE (Rtsne library)
+Rtsne_fn <- function(pca_results) {
+  set.seed(tsne_seed)
+  tsne_df <- Rtsne(pca_results, dims = tsne_dims, perplexity = tsne_perplexity, max_iter = tsne_max_iter, check_duplicate = FALSE, pca = FALSE)
+  return(tsne_df)
+}
+
+# Function that performs t-SNE (tsne library)
 tsne_fn <- function(pca_results) {
   set.seed(tsne_seed)
-  tsne_df <- Rtsne(pca_results, dims = tsne_dims, perplexity = tsne_perplexity, max_iter = tsne_max_iter, check_duplicates = FALSE, pca = FALSE)
+  tsne_df <- tsne(pca_results, k = tsne_dims, initial_dims = tsne_initial_dims, perplexity = tsne_perplexity, max_iter = tsne_max_iter)
   return(tsne_df)
 }
 
@@ -132,36 +129,73 @@ umap_plot <- function(umap_df, target, k) {
 # -----START-----
 
 # Set k for k-mer analysis
-k <- 7
+k <- 3
 
 # Set paths
 data_path <- "./data/archive/"
+
 results_path <- "./results/dim-reduce/R/"
 
+# Check if the directory already exists
+if (!dir.exists(results_path)) {
+  # Create the directory if it doesn't exist
+  dir.create(results_path, recursive = TRUE)
+}
+
 # Loading the data file
-data <- read.csv(paste0(data_path, "kmer_", k, ".csv"))
+raw_data <- read.csv(paste0(data_path, "kmer_", k, ".csv"))
 
 # Define t-SNE and UMAP parameters
 tsne_seed <- 10
 tsne_dims <- 2
 tsne_perplexity <- 40
 tsne_max_iter <- 300
+tsne_initial_dims <- 50
 umap_seed <- 10
 umap_n_neighbors <- 15
 umap_metric <- "euclidean"
 umap_min_dist <- 0.1
 
 # Process the data
-data <- process_data(data)
+data <- process_data(raw_data)
 
 # Perform PCA
 pca_df <- pca_fn(data$x)
 
 # Plot PCA results
-pca_plot(pca_df, data$target, k)
+pca_plot(pca_df, raw_data, k)
 
-# Perform t-SNE using PCA results
-tsne_df <- tsne_fn(pca_df$x)
+# Create screeplot
+p <- fviz_eig(pca_df)
+save_plot("screeplot", k, p)
+
+# Create graph of individuals
+p <- fviz_pca_ind(pca_df, 
+                  col.ind = "cos2", # Color by the quality of representation
+                  gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07")
+                  )
+save_plot("indivgraph", k, p)
+
+# Create graph of variables
+p <- fviz_pca_var(pca_df,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07")
+)
+save_plot("vargraph", k, p)
+
+# Create biplot of individuals and variables
+p <- fviz_pca_biplot(pca_df,
+                col.var = "#2E9FDF", # Variables color
+                col.ind = "#696969"  # Individuals color
+)
+save_plot("biplot", k, p)
+
+# Perform t-SNE(Rtsne library) using PCA results
+tsne_df <- Rtsne_fn(pca_df$x)
+
+# # Perform t-SNE(tsne Library) using PCA results
+# TO DO: Check/Fix params needeed (Read docu)
+# tsne_df <- tsne_fn(pca_df$x)
 
 # Plot t-SNE results
 tsne_plot(tsne_df, data$target, k)
