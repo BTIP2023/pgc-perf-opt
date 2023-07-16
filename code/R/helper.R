a2db4bd7447435e072ba0dbc1b604d9dc45350b3
YSHebron <yshebron@up.edu.ph>
2023-07-16 13:59:48 +0800

    fix: prev commit broken, patched helper.R paramsLog(), added writeback for non Windows/Linux systems

--- code/R/helper.R
+++ code/R/helper.R
@@ -8,29 +8,36 @@ library(pacman)
 
 # Write system information and parameters to a text file specified in
 # output_path. Creates text file in output_path if not exists.
-# Overwrites if exists. Only works for Windows and Linux.
+# Overwrites if exists. Logger only supports Windows 10/11 and Linux.
 # Assumes that there's only one processor installed.
 paramsLog <- function(outputDir, filename, paramString) {
-  if (!dir.exists(outputDir)) dir.create(outputDir)
+  if (!dir.exists(outputDir))
+    dir.create(outputDir)
   output_path <- paste(outputDir, filename, sep = '/')
   fileConn<-file(output_path)
   if (pacman::p_detectOS() == 'Windows') {
-    specs <- system('systeminfo', intern=TRUE)[
-      c(3,4,8,13,14,15,16,17,length(specs))]
-    # WMIC CPU Get DeviceID,NumberOfCores,NumberOfLogicalProcessors
-    # Get-WmiObject win32_VideoController
+    systeminfo <- system('systeminfo', intern=TRUE)
+    systeminfo <- systeminfo[c(3,4,13,14,15,16,17,25)]
+    cpuinfo <- system('WMIC CPU Get DeviceID,NumberOfCores,NumberOfLogicalProcessors',
+                      intern = TRUE)
+    cpuinfo <- cpuinfo[-length(cpuinfo)]
+    specs <- paste(systeminfo, cpuinfo)
     write_lines(c(as.character(Sys.time()), specs, paramString,
                   '------\n'), output_path, append=TRUE)
-  } else {
+  } else if (pacman::p_detectOS() == 'Linux') {
     hostnamectl <- trimws(system('hostnamectl', intern=T)[c(6,7,9,10)])
     lsb_release <- system('lsb_release -a', intern=T)
-    lsb_release <- gsub('\\t', ' ', lsb_release[length(lsb_release)])
+    lsb_release <- lsb_release[length(lsb_release)]
     lscpu <- system('lscpu', intern=T)[c(1,5,8,11,16,17,30,33,34,35)]
     mem <- system('grep MemTotal /proc/meminfo', intern=T)
     specs <- c(hostnamectl, lsb_release, lscpu, mem)
     write_lines(c(as.character(Sys.time()), specs, paramString,
                   '------\n'), output_path, append=TRUE)
-  } 
+  } else {
+    write_lines(c(as.character(Sys.time()),
+                  "OS not supported by logger!", paramString,
+                  '------\n'), output_path, append=TRUE)
+  }
   close(fileConn)
 }
 
